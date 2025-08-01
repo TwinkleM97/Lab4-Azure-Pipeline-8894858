80ca664bc72097af69341bf71109e6b2312b5258
Twinkle Mishra <mishrtwinkle1997@gmail.com>
2025-08-01 18:45:59 -0400

    updated index.js

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -1,10 +1,8 @@
 module.exports = async function (context, req) {
     context.log('HTTP trigger function processed a request.');
 
-    const name = req.query.name || req.body?.name;
-    const responseMessage = name
-        ? `Hello, ${name}! This Node.js Azure Function is working correctly.`
-        : "Hello, World! This Node.js Azure Function is working correctly.";
+    const name = req?.query?.name || req?.body?.name || 'World';
+    const responseMessage = `Hello, ${name}! This Node.js Azure Function is working correctly.`;
 
     context.res = {
         status: 200,
